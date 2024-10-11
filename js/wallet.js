c7c83486675c4d7b3c27ab476936a1f927d8283b
rbriphy <rbriphy@gmail.com>
2024-10-11 15:12:32 -0400

    changed ethers reference

--- js/wallet.js
+++ js/wallet.js
@@ -1,6 +1,6 @@
 import { CONTRACT_ADDRESS, EXPECTED_CHAIN_ID, EXPECTED_NETWORK_NAME } from './config.js';
 import { contractABI } from './abi.js';
-import { BrowserProvider } from 'ethers';
+import { BrowserProvider } from '../node_modules/ethers';
 import * as sapphire from '../node_modules/@oasisprotocol/sapphire-paratime';
 
 let signer;
